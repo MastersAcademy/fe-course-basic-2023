6f93940b05509595178a896dc95cdc7a05c51bdc
olena_ambitska <lambitska92@gmail.com>
2023-10-26 20:28:51 +0300

    fix: added values

--- homeworks/olena.ambitska_elena-ambitska/4-basics/basics.js
+++ homeworks/olena.ambitska_elena-ambitska/4-basics/basics.js
@@ -4,12 +4,12 @@ const FIELD = document.getElementById('field');
 
 const TEMPLATE = `
     <ul>
-        <li><b>Additional operation: </b>${VALUE_FIRST + VALUE_SECOND};</li>
-        <li><b>Substract operation: </b>${VALUE_FIRST - VALUE_SECOND};</li>
-        <li><b>Multiplication  operation: </b>${VALUE_FIRST * VALUE_SECOND};</li>
-        <li><b>Division  operation: </b>${(VALUE_FIRST / VALUE_SECOND).toFixed(2)};</li>
-        <li><b>Modulo: </b>${VALUE_FIRST % VALUE_SECOND};</li>
-        <li><b>Exponentiation operation: </b>${VALUE_FIRST ** VALUE_SECOND};</li>
+        <li><b>Additional operation: </b>${VALUE_FIRST} + ${VALUE_SECOND} = ${VALUE_FIRST + VALUE_SECOND};</li>
+        <li><b>Substract operation: </b>${VALUE_FIRST} - ${VALUE_SECOND} =${VALUE_FIRST - VALUE_SECOND};</li>
+        <li><b>Multiplication  operation: </b>${VALUE_FIRST} * ${VALUE_SECOND} = ${VALUE_FIRST * VALUE_SECOND};</li>
+        <li><b>Division  operation: </b>${VALUE_FIRST} / ${VALUE_SECOND} = ${(VALUE_FIRST / VALUE_SECOND).toFixed(2)};</li>
+        <li><b>Modulo: </b>${VALUE_FIRST} % ${VALUE_SECOND} = ${VALUE_FIRST % VALUE_SECOND};</li>
+        <li><b>Exponentiation operation: </b>${VALUE_FIRST} ** ${VALUE_SECOND} = ${VALUE_FIRST ** VALUE_SECOND};</li>
     </ul>
 `;
 
