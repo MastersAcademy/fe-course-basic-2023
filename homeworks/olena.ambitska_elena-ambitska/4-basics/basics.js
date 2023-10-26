2f47cc5044cb38f07ad3f763e15b4a6a7b4e0454
olena_ambitska <lambitska92@gmail.com>
2023-10-26 18:48:58 +0300

    feat: added basics operations

---
+++ homeworks/olena.ambitska_elena-ambitska/4-basics/basics.js
@@ -0,0 +1,16 @@
+const VALUE_FIRST = 5;
+const VALUE_SECOND = 7;
+const FIELD = document.getElementById('field');
+
+const TEMPLATE = `
+    <ul>
+        <li><b>Additional operation: </b>${VALUE_FIRST + VALUE_SECOND};</li>
+        <li><b>Substract operation: </b>${VALUE_FIRST - VALUE_SECOND};</li>
+        <li><b>Multiplication  operation: </b>${VALUE_FIRST * VALUE_SECOND};</li>
+        <li><b>Division  operation: </b>${(VALUE_FIRST / VALUE_SECOND).toFixed(2)};</li>
+        <li><b>Modulo: </b>${VALUE_FIRST % VALUE_SECOND};</li>
+        <li><b>Exponentiation operation: </b>${VALUE_FIRST ** VALUE_SECOND};</li>
+    </ul>
+`;
+
+FIELD.insertAdjacentHTML('beforeend', TEMPLATE);
