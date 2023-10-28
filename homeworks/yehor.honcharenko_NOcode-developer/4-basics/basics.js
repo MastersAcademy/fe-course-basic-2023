720dd53ab293814006c706776884476b2c066fbd
NOcode-developer <yehor.design@gmail.com>
2023-10-28 22:07:09 +0300

    hw4-- basics, mathematics
    1. Created folder "4-basics"
    1.1 basic.js
    1.2 index.html
    1.3 basic.css
    
    2. Created some basic math operation in .js
    3. Connected .js and .css to the html
    4. Displaying data through the console

---
+++ homeworks/yehor.honcharenko_NOcode-developer/4-basics/basics.js
@@ -0,0 +1,17 @@
+let firstNumber = 0;
+let secondNumber = 7;
+let thirdNumber = 73;
+
+let x1 = firstNumber + thirdNumber;
+let x2 = secondNumber - firstNumber;
+let x3 = firstNumber * secondNumber;
+let x4 = secondNumber / firstNumber;
+let x5 = secondNumber % thirdNumber;
+let x6 = secondNumber ** thirdNumber;
+
+console.log(`Operation addition: ${firstNumber} + ${thirdNumber} = ${x1}`);
+console.log(`Operation addition: ${secondNumber} - ${firstNumber} = ${x2}`);
+console.log(`Operation addition: ${firstNumber} * ${secondNumber} = ${x3}`);
+console.log(`Operation addition: ${secondNumber} / ${firstNumber} = ${x4}`);
+console.log(`Operation addition: ${secondNumber} % ${thirdNumber} = ${x5}`);
+console.log(`Operation addition: ${secondNumber} ** ${thirdNumber} = ${x6}`);
