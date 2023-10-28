fe0f9aeefa12d027ccb184b7bca255c8b61470e3
Mariya Artemenko <mariya.artemenko1604@gmail.com>
2023-10-28 20:41:47 +0300

    corrections after linters checks

--- homeworks/mariya.artemenko_Mariya-Artemenko/4-basics/basics.js
+++ homeworks/mariya.artemenko_Mariya-Artemenko/4-basics/basics.js
@@ -1,12 +1,12 @@
-const firstNumber = Number(prompt("Please, fill the first number:"));
-const secondNumber = Number(prompt("Please, fill the second number:"));
+const firstNumber = Number(prompt('Please, fill the first number:'));
+const secondNumber = Number(prompt('Please, fill the second number:'));
 
 const resultAddition = firstNumber + secondNumber;
 const resultSubtraction = firstNumber - secondNumber;
 const resultMultiplication = firstNumber * secondNumber;
 const resultDivision = firstNumber / secondNumber;
 const resultDivisionWithRemainder = firstNumber % secondNumber;
-const  resultExponentiation = firstNumber ** secondNumber;
+const resultExponentiation = firstNumber ** secondNumber;
 
 alert(`Operation addition: ${firstNumber} + ${secondNumber} = ${resultAddition}`);
 alert(`Operation subtraction: ${firstNumber} - ${secondNumber} = ${resultSubtraction}`);
@@ -16,19 +16,19 @@ alert(`Operation division with remainder: ${firstNumber} % ${secondNumber} = ${r
 alert(`Operation exponentiation: ${firstNumber} ** ${secondNumber} = ${resultExponentiation}`);
 
 function showResult() {
-    const resultAdditionEl = document.querySelector("[data-result1]");
-    const resultSubtractionEL = document.querySelector("[data-result2]");
-    const resultMultiplicationEL = document.querySelector("[data-result3]");
-    const resultDivisionEL = document.querySelector("[data-result4]");
-    const resultDivisionWithRemainderEL = document.querySelector("[data-result5]");
-    const resultExponentiationEL = document.querySelector("[data-result6]");
+    const resultAdditionEl = document.querySelector('[data-result1]');
+    const resultSubtractionEL = document.querySelector('[data-result2]');
+    const resultMultiplicationEL = document.querySelector('[data-result3]');
+    const resultDivisionEL = document.querySelector('[data-result4]');
+    const resultDivisionWithRemainderEL = document.querySelector('[data-result5]');
+    const resultExponentiationEL = document.querySelector('[data-result6]');
 
-    resultAdditionEl.textContent =  `Operation addition: ${firstNumber} + ${secondNumber} = ${resultAddition}`;
-    resultSubtractionEL.textContent =  `Operation subtraction: ${firstNumber} - ${secondNumber} = ${resultSubtraction}`;
-    resultMultiplicationEL.textContent =  `Operation multiplication: ${firstNumber} * ${secondNumber} = ${resultMultiplication}`;
-    resultDivisionEL.textContent =  `Operation division: ${firstNumber} + ${secondNumber} = ${resultDivision}`;
-    resultDivisionWithRemainderEL.textContent =  `Operation division with remainder: ${firstNumber} % ${secondNumber} = ${resultDivisionWithRemainder}`;
-    resultExponentiationEL.textContent =  `Operation exponentiation: ${firstNumber} ** ${secondNumber} = ${resultExponentiation}`;
+    resultAdditionEl.textContent = `Operation addition: ${firstNumber} + ${secondNumber} = ${resultAddition}`;
+    resultSubtractionEL.textContent = `Operation subtraction: ${firstNumber} - ${secondNumber} = ${resultSubtraction}`;
+    resultMultiplicationEL.textContent = `Operation multiplication: ${firstNumber} * ${secondNumber} = ${resultMultiplication}`;
+    resultDivisionEL.textContent = `Operation division: ${firstNumber} + ${secondNumber} = ${resultDivision}`;
+    resultDivisionWithRemainderEL.textContent = `Operation division with remainder: ${firstNumber} % ${secondNumber} = ${resultDivisionWithRemainder}`;
+    resultExponentiationEL.textContent = `Operation exponentiation: ${firstNumber} ** ${secondNumber} = ${resultExponentiation}`;
 }
 
 showResult();
