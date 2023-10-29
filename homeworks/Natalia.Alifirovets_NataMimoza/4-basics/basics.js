22808d18c46eaf7110645ca197c09d5fe98e824c
NataMimoza <natastalkant@gmail.com>
2023-10-29 19:18:57 +0200

    Added new work

---
+++ homeworks/Natalia.Alifirovets_NataMimoza/4-basics/basics.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 11;
+const c = 20;
+
+const additional = b + c;
+const subtraction = b - a;
+const multiplication = a * b;
+const divide = c / a;
+const remainder = c % a;
+const power = b ** a;
+
+console.log('Additional:', additional);
+console.log('Subtraction:', subtraction);
+console.log('Multiplication:', multiplication);
+console.log('Divide:', divide);
+console.log('Remainder:', remainder);
+console.log('Power:', power);
