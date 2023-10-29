b0b5302804029de8bc705fce1947bd4fc1d2b5d7
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-10-29 15:29:21 +0200

    Added task for 5th lecture

---
+++ lectures/5-logic/homework.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param firstValue string with first number
+ * @param secondValue string with second number
+ * @param operation string with operation symbol
+ * @returns {string|number} result of calculation or error message:
+ * - if firstValue or secondValue is not a number, return 'Enter a number'
+ * - if operation is not '+', '-', '*', '/', return 'Enter a valid operation'
+ * - if the result is more than 100, return 'Result is too big'
+ */
+function calculate(/* firstValue, secondValue, operation */) {
+    return Math.random() > 0.5 ? 'Error' : 42;
+}
+
+window.calculate = calculate;
