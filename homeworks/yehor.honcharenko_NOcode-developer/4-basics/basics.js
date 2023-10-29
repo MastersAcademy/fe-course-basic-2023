020ed2fad16ef17e24b5cee197ad6849911f1253
NOcode-developer <yehor.design@gmail.com>
2023-10-29 21:36:48 +0200

    fix-- Improved readability and simplified code

--- homeworks/yehor.honcharenko_NOcode-developer/4-basics/basics.js
+++ homeworks/yehor.honcharenko_NOcode-developer/4-basics/basics.js
@@ -2,16 +2,9 @@ const firstNumber = 0;
 const secondNumber = 7;
 const thirdNumber = 73;
 
-const x1 = firstNumber + thirdNumber;
-const x2 = secondNumber - firstNumber;
-const x3 = firstNumber * secondNumber;
-const x4 = secondNumber / firstNumber;
-const x5 = secondNumber % thirdNumber;
-const x6 = secondNumber ** thirdNumber;
-
-console.log(`Operation addition: ${firstNumber} + ${thirdNumber} = ${x1}`);
-console.log(`Operation addition: ${secondNumber} - ${firstNumber} = ${x2}`);
-console.log(`Operation addition: ${firstNumber} * ${secondNumber} = ${x3}`);
-console.log(`Operation addition: ${secondNumber} / ${firstNumber} = ${x4}`);
-console.log(`Operation addition: ${secondNumber} % ${thirdNumber} = ${x5}`);
-console.log(`Operation addition: ${secondNumber} ** ${thirdNumber} = ${x6}`);
+console.log(`Operation addition: ${firstNumber} + ${thirdNumber} = ${firstNumber + thirdNumber}`);
+console.log(`Operation subtraction: ${secondNumber} - ${firstNumber} = ${secondNumber - firstNumber}`);
+console.log(`Operation multiplication: ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`Operation division: ${secondNumber} / ${firstNumber} = ${secondNumber / firstNumber}`);
+console.log(`Operation remainder: ${secondNumber} % ${thirdNumber} = ${secondNumber % thirdNumber}`);
+console.log(`Operation exponentiation: ${secondNumber} ** ${thirdNumber} = ${secondNumber ** thirdNumber}`);
