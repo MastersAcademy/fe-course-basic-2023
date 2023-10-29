33ebb3b128f9f843e9437f3cb8dbade35b07b572
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-10-29 15:42:01 +0200

    Corrected an error message

--- lectures/5-logic/homework.js
+++ lectures/5-logic/homework.js
@@ -5,7 +5,7 @@
  * @param operation string with operation symbol
  * @returns {string|number} result of calculation or error message:
  * - if firstValue or secondValue is not a number, return 'Enter a number'
- * - if operation is not '+', '-', '*', '/', return 'Enter a valid operation'
+ * - if operation is not '+', '-', '*', '/', return 'Choose a valid operation'
  * - if the result is more than 100, return 'Result is too big'
  */
 function calculate(/* firstValue, secondValue, operation */) {
