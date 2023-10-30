b5d235581a02d689c0bdaf5f98f137b758cca7b4
unknown <romanpavlyuk98@gmail.com>
2023-10-30 13:21:03 +0200

    HW 4 - JS Basics__RomanPvlk + fixed login_header in previous HW + fixed linters

--- homeworks/roman.pavliuk_RomanPvlk/4-basics/basics.js
+++ homeworks/roman.pavliuk_RomanPvlk/4-basics/basics.js
@@ -18,17 +18,17 @@ console.log(`Operation modulo: ${numberFirst} % ${numberSecond} = ${moduloOperat
 console.log(`Operation exponentation: ${numberFirst} ** ${numberSecond} = ${exponentationOperation}`);
 
 // Отримання посилань на HTML-елементи, де будуть відображатись результати
-const additionOperationElement = document.getElementById("additionOperation");
-const substractionOperationElement = document.getElementById("substractionOperation");
-const multiplicationOperationElement = document.getElementById("multiplicationOperation");
-const divisionOperationElement = document.getElementById("divisionOperation");
-const moduloOperationElement = document.getElementById("moduloOperation");
-const exponentationOperationElement = document.getElementById("exponentationOperation");
+const additionOperationElement = document.getElementById('additionOperation');
+const substractionOperationElement = document.getElementById('substractionOperation');
+const multiplicationOperationElement = document.getElementById('multiplicationOperation');
+const divisionOperationElement = document.getElementById('divisionOperation');
+const moduloOperationElement = document.getElementById('moduloOperation');
+const exponentationOperationElement = document.getElementById('exponentationOperation');
 
 // Вставка результатів у відповідні HTML-елементи
-additionOperationElement.textContent = 'Результат додавання: ' + (` ${numberFirst} + ${numberSecond} = ${additionOperation}`);
-substractionOperationElement.textContent = 'Результат віднімання: ' + (` ${numberFirst} - ${numberSecond} = ${substractionOperation}`);
-multiplicationOperationElement.textContent = 'Результат множення: ' + (` ${numberFirst} * ${numberSecond} = ${multiplicationOperation}`);
-divisionOperationElement.textContent = 'Результат ділення: ' + (` ${numberFirst} / ${numberSecond} = ${divisionOperation}`);
-moduloOperationElement.textContent = 'Отримання залишку від ділення: ' + (` ${numberFirst} % ${numberSecond} = ${moduloOperation}`);
-exponentationOperationElement.textContent = 'Піднесення до степення: ' + (` ${numberFirst} ** ${numberSecond} = ${exponentationOperation}`);
+additionOperationElement.textContent = `Результат додавання: ${numberFirst} + ${numberSecond} = ${additionOperation}`;
+substractionOperationElement.textContent = `Результат віднімання: ${numberFirst} - ${numberSecond} = ${substractionOperation}`;
+multiplicationOperationElement.textContent = `Результат множення: ${numberFirst} * ${numberSecond} = ${multiplicationOperation}`;
+divisionOperationElement.textContent = `Результат ділення: ${numberFirst} / ${numberSecond} = ${divisionOperation}`;
+moduloOperationElement.textContent = `Отримання залишку від ділення: ${numberFirst} % ${numberSecond} = ${moduloOperation}`;
+exponentationOperationElement.textContent = `Піднесення до степення: ${numberFirst} ** ${numberSecond} = ${exponentationOperation}`;
