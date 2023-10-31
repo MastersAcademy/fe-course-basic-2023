3ce5dea0dd7e0991fa77ad1fad88496d635d3ee1
Allocene <artur.kobylatski@gmail.com>
2023-11-01 00:43:17 +0200

    added files, realized js logic + style

---
+++ homeworks/artur.kobyliatsky_Allocene/5-logic/homework.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * @param firstValue string with first number
+ * @param secondValue string with second number
+ * @param operation string with operation symbol
+ * @returns {string|number} result of calculation or error message:
+ * - if firstValue or secondValue is not a number, return 'Enter a number'
+ * - if operation is not '+', '-', '*', '/', return 'Choose a valid operation'
+ * - if the result is more than 100, return 'Result is too big'
+ * - else return number of result: 1 '+' 2 returns 3
+ *
+*/
+
+function calculate(firstValue, secondValue, operation) {
+    const num1 = Number(firstValue);
+    const num2 = Number(secondValue);
+
+    if (Number.isNaN(num1) || Number.isNaN(num2)) {
+        return 'Enter a number';
+    }
+
+    let result;
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                return 'Division by zero is not allowed';
+            }
+            result = num1 / num2;
+            break;
+        default:
+            return 'Choose a valid operation';
+    }
+
+    if (result > 100) {
+        return 'Result is too big';
+    }
+
+    return result;
+}
+
+window.calculate = calculate;
