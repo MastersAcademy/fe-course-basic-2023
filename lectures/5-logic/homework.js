9e43896f7ecc1f159300e1cb4d455c4126c34781
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-10-31 20:15:57 +0200

    Added some comment.

--- lectures/5-logic/homework.js
+++ lectures/5-logic/homework.js
@@ -7,8 +7,11 @@
  * - if firstValue or secondValue is not a number, return 'Enter a number'
  * - if operation is not '+', '-', '*', '/', return 'Choose a valid operation'
  * - if the result is more than 100, return 'Result is too big'
+ * - else return number of result: 1 '+' 2 returns 3
+ *
  */
 function calculate(/* firstValue, secondValue, operation */) {
+    // TODO: write your code here, remove the line below and uncomment the lines above
     return Math.random() > 0.5 ? 'Error' : 42;
 }
 
