6dd9b6d531127b8d444a7b9c6a9e0e1394145205
Lytvynenko18 <roman0672232679@gmail.com>
2023-10-31 17:22:57 +0200

    home_work4

---
+++ homeworks/roman.lytvynenko_Lytvynenko18/4-basics/basics.js
@@ -0,0 +1,16 @@
+const number1 = 20;
+const number2 = 5;
+
+const addition = number1 + number2;
+const subtraction = number1 - number2;
+const multiplication = number1 * number2;
+const exponentiation = number1 ** number2;
+const division = number1 / number2;
+const modulus = number1 % number2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(exponentiation);
+console.log(division);
+console.log(modulus);
