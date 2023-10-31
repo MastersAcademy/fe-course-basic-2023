21d43f41465df8e684868ef2f0ec2564540ce28c
Lytvynenko18 <roman0672232679@gmail.com>
2023-10-31 18:04:38 +0200

    home_work4

--- homeworks/roman.lytvynenko_Lytvynenko18/4-basics/basics.js
+++ homeworks/roman.lytvynenko_Lytvynenko18/4-basics/basics.js
@@ -8,9 +8,16 @@ const exponentiation = number1 ** number2;
 const division = number1 / number2;
 const modulus = number1 % number2;
 
-console.log(addition);
-console.log(subtraction);
-console.log(multiplication);
-console.log(exponentiation);
-console.log(division);
-console.log(modulus);
+console.log(`Addition: ${number1} + ${number2} = ${addition}`);
+console.log(`Subtraction: ${number1} - ${number2} = ${subtraction}`);
+console.log(`Multiplication: ${number1} * ${number2} = ${multiplication}`);
+console.log(`Exponentiation: ${number1} ** ${number2} = ${exponentiation}`);
+console.log(`Division: ${number1} / ${number2} = ${division}`);
+console.log(`Modulus: ${number1} %${number2} = ${modulus}`);
+
+document.write(`<p class="paragraph"> Addition: ${number1} + ${number2} = ${addition} </p>`);
+document.write(`<p class="paragraph">Subtraction: ${number1} - ${number2} = ${subtraction} </p>`);
+document.write(`<p class="paragraph">Multiplication: ${number1} * ${number2} = ${multiplication} </p>`);
+document.write(`<p class="paragraph">Exponentiation: ${number1} ** ${number2} = ${exponentiation} </p>`);
+document.write(`<p class="paragraph">Division: ${number1} / ${number2} = ${division} </p>`);
+document.write(`<p class="paragraph">Modulus: ${number1} %${number2} = ${modulus} </p>`);
