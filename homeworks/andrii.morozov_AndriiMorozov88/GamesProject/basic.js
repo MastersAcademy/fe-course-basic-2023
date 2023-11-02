5b6334413166ee5c9b3faf95b044352259b7e05f
Andrii Morozov <slon88888@ukr.net>
2023-11-02 23:02:31 +0200

    calculator without cards

--- homeworks/andrii.morozov_AndriiMorozov88/GamesProject/basic.js
+++ homeworks/andrii.morozov_AndriiMorozov88/GamesProject/basic.js
@@ -3,89 +3,88 @@ const secondNumberInput = document.querySelector('[data-second-number]');
 const operation = document.querySelector('[data-operation]');
 const button = document.querySelector('[data-button]');
 const resultContainer = document.querySelector('[data-result]');
-const errorContainer = document.querySelector('[data-error]');
-const cardContainer = document.querySelector('[data-card-container]');
+const commentContainer = document.querySelector('[data-comment]');
 resultContainer.innerText = 'Result';
 function getResultObject() {
     const firstNumber = Number(firstNumberInput.value);
     const secondNumber = Number(secondNumberInput.value);
-    let result;
     const resultObject = {};
     resultObject.firstNumber = firstNumber;
     resultObject.secondNumber = secondNumber;
-    resultObject.totalInputColumns = Math.ceil(firstNumber / 10);
-    resultObject.lastInputColumn = firstNumber - Math.floor(firstNumber / 10) * 10;
     switch (operation.value) {
         case '1':
-            result = firstNumber + secondNumber;
+            resultObject.result = firstNumber + secondNumber;
+            resultObject.operator = 1;
             break;
         case '2':
-            result = firstNumber - secondNumber;
+            resultObject.result = firstNumber - secondNumber;
+            resultObject.operator = 2;
             break;
         case '3':
-            result = firstNumber * secondNumber;
+            resultObject.result = firstNumber * secondNumber;
+            resultObject.operator = 3;
             break;
         case '4':
-            result = firstNumber / secondNumber;
+            resultObject.result = firstNumber / secondNumber;
+            resultObject.operator = 4;
             break;
         default:
-            result = 'Error';
+            resultObject.result = 'Error';
             resultObject.error = 1;
             break;
     }
     if (Number.isNaN(firstNumber)) {
-        result = 'Error';
+        resultObject.result = 'Error';
         resultObject.error = 2;
     }
     if (Number.isNaN(secondNumber)) {
-        result = 'Error';
+        resultObject.result = 'Error';
         resultObject.error = 3;
     }
-    if (result > 100) {
-        result = 'Error';
+    if (resultObject.result > 100) {
+        resultObject.result = 'Error';
         resultObject.error = 4;
     }
-    resultObject.operationResult = result;
-    resultObject.totalResultColumns = Math.ceil(result / 10);
-    resultObject.lastResultColumn = result - Math.floor(result / 10) * 10;
     return resultObject;
 }
-function showCardColumn(number) {
-    let cardStep = 0;
-    for (let item = 0; item < number; item++) {
-        const card = document.createElement('div');
-        cardContainer.append(card);
-        card.classList.add('card');
-        card.style.top = `${cardStep}px`;
-        cardStep += 15;
-    }
-}
 function showResult() {
-    errorContainer.classList.add('error--hidden');
+    commentContainer.classList.add('comment--hidden');
     resultContainer.classList.remove('calculator__result--error');
     const resultObject = getResultObject();
     if (resultObject.error) {
-        errorContainer.classList.remove('error--hidden');
+        commentContainer.classList.remove('comment--hidden');
         resultContainer.classList.add('calculator__result--error');
         switch (resultObject.error) {
             case 1:
-                errorContainer.innerText = 'operation is not supported';
+                commentContainer.innerText = 'operation is not supported';
                 break;
             case 2:
-                errorContainer.innerText = 'first operand is not a number';
+                commentContainer.innerText = 'first operand is not a number';
                 break;
             case 3:
-                errorContainer.innerText = 'second operand is not a number';
+                commentContainer.innerText = 'second operand is not a number';
                 break;
             default:
-                errorContainer.innerText = 'number is too big';
+                commentContainer.innerText = 'number is too big';
                 break;
         }
     } else {
-        showCardColumn(resultObject.lastInputColumn);
+        switch (resultObject.operator) {
+            case 1:
+                commentContainer.innerText = `${resultObject.firstNumber} + ${resultObject.secondNumber} = ${resultObject.result}`;
+                break;
+            case 2:
+                commentContainer.innerText = `${resultObject.firstNumber} - ${resultObject.secondNumber} = ${resultObject.result}`;
+                break;
+            case 3:
+                commentContainer.innerText = `${resultObject.firstNumber} * ${resultObject.secondNumber} = ${resultObject.result}`;
+                break;
+            default:
+                commentContainer.innerText = `${resultObject.firstNumber} / ${resultObject.secondNumber} = ${resultObject.result}`;
+                break;
+        }
     }
-    resultContainer.innerText = `${resultObject.operationResult}`;
+    resultContainer.innerText = `${resultObject.result}`;
     resultContainer.classList.add('calculator__result--calc');
-    console.log(resultObject);
 }
 button.addEventListener('click', showResult);
