fc2fede148798401dea04ade7fe45880e0511a08
MarkoAndrew93 <112160947+MarkoAndrew93@users.noreply.github.com>
2023-11-02 20:42:00 +0300

    Homework 5

---
+++ homeworks/andrew.markovskiy_MarkoAndrew93/5-logic/homework.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * @param firstValue string with first number
+ * @param secondValue string with second number
+ * @param operation string with operation symbol
+ * @returns {string|number} result of calculation or error message:
+ * - if firstValue or secondValue is not a number, return 'Enter a number'
+ * - if operation is not '+', '-', '*', '/', return 'Choose a valid operation'
+ * - if the result is more than 100, return 'Result is too big'
+ * - else return number of result: 1 '+' 2 returns 3
+ *
+ */
+function calculate(firstValue, secondValue, operation) {
+  if (isNaN(firstValue) || isNaN(secondValue)) {
+    return 'Enter a number';
+  }
+
+  if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
+    return 'Choose a valid operation';
+  }
+
+  let result;
+    switch (operation) {
+      case '+':
+        result = parseFloat(firstValue) + parseFloat(secondValue);
+        break;
+      case '-':
+        result = parseFloat(firstValue) - parseFloat(secondValue);
+        break;
+      case '*':
+        result = parseFloat(firstValue) * parseFloat(secondValue);
+        break;
+      case '/':
+        result = parseFloat(firstValue) / parseFloat(secondValue);
+        break;
+    }
+    
+    if (result > 100) {
+      return 'Result is too big';
+    }
+    
+    return result;
+  }
+  
+window.calculate = calculate;
---
+++ homeworks/andrew.markovskiy_MarkoAndrew93/5-logic/script.js
@@ -0,0 +1,13 @@
+const FIRST_VALUE_ELEMENT = document.getElementById('firstValue');
+const SECOND_VALUE_ELEMENT = document.getElementById('secondValue');
+const OPERATION_ELEMENT = document.getElementById('operation');
+const CALCULATE_BUTTON_ELEMENT = document.getElementById('calculate');
+const RESULT_ELEMENT = document.getElementById('result');
+
+CALCULATE_BUTTON_ELEMENT.addEventListener('click', () => {
+    const firstValue = FIRST_VALUE_ELEMENT.value;
+    const secondValue = SECOND_VALUE_ELEMENT.value;
+    const operation = OPERATION_ELEMENT.value;
+
+    RESULT_ELEMENT.innerText = window.calculate(firstValue, secondValue, operation);
+});
