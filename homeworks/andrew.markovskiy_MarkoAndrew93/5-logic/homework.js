fb866258d1324b7820278330eff4c22c19fa0878
MarkoAndrew93 <112160947+MarkoAndrew93@users.noreply.github.com>
2023-11-02 21:02:49 +0300

    fixed Homework 5

--- homeworks/andrew.markovskiy_MarkoAndrew93/5-logic/homework.js
+++ homeworks/andrew.markovskiy_MarkoAndrew93/5-logic/homework.js
@@ -11,36 +11,36 @@
  *
  */
 function calculate(firstValue, secondValue, operation) {
-    if (Number.isNaN(Number(firstValue)) || Number.isNaN(Number(secondValue))) {
-        return 'Enter a number';
-      }
-      
+        if (Number.isNaN(Number(firstValue)) || Number.isNaN(Number(secondValue))) {
+            return 'Enter a number';
+        }
+        
+        if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
+        return 'Choose a valid operation';
+        }
 
-    if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
-      return 'Choose a valid operation';
-    }
+        let result;
+        switch (operation) {
+            case '+':
+            result = parseFloat(firstValue) + parseFloat(secondValue);
+            break;
+            case '-':
+            result = parseFloat(firstValue) - parseFloat(secondValue);
+            break;
+            case '*':
+            result = parseFloat(firstValue) * parseFloat(secondValue);
+            break;
+            case '/':
+            result = parseFloat(firstValue) / parseFloat(secondValue);
+            break;
+            default: ; 
+        }
 
-    let result;
-      switch (operation) {
-        case '+':
-          result = parseFloat(firstValue) + parseFloat(secondValue);
-          break;
-        case '-':
-          result = parseFloat(firstValue) - parseFloat(secondValue);
-          break;
-        case '*':
-          result = parseFloat(firstValue) * parseFloat(secondValue);
-          break;
-        case '/':
-          result = parseFloat(firstValue) / parseFloat(secondValue);
-          break;
-      }
-    
-      if (result > 100) {
-        return 'Result is too big';
-      }
-    
-      return result;
-    }
-  
+        if (result > 100) {
+            return 'Result is too big';
+        }
+        
+        return result;
+        }
+
 window.calculate = calculate;
