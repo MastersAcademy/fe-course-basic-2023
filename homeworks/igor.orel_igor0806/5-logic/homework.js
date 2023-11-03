fa28acb9a735771d8a5644c50a7b549833b9290e
Orel Igor <igor.orel0806@gmail.com>
2023-11-04 00:53:15 +0200

    5-logic calculator v1; add function and style; igor0806

---
+++ homeworks/igor.orel_igor0806/5-logic/homework.js
@@ -0,0 +1,51 @@
+/**
+ * Perform a mathematical operation on two numbers. Return result or error.
+ * @param firstValue {string} with first number
+ * @param secondValue {string} with second number
+ * @param operation {string} with operation symbol
+ * @returns {string|number} result of calculation or error message:
+ * - if firstValue or secondValue is not a number, return 'Enter a number'
+ * - if operation is not '+', '-', '*', '/', return 'Choose a valid operation'
+ * - if the result is more than 100, return 'Result is too big'
+ * - else return number of result: 1 '+' 2 returns 3
+ *
+ */
+function calculate(firstValue, secondValue, operation) {
+    const firstNum = Number(firstValue);
+    const secondNum = Number(secondValue);
+    let result;
+
+    if (operation === '+') {
+        result = firstNum + secondNum;
+    }
+
+    if (operation === '-') {
+        result = firstNum - secondNum;
+    }
+
+    if (operation === '/') {
+        result = firstNum / secondNum;
+    }
+
+    if (operation === '*') {
+        result = firstNum * secondNum;
+    } else {
+        return 'Choose a valid operation';
+    }
+
+    if (firstValue.length === 0 || secondValue.length === 0) {
+        return 'Enter a number';
+    }
+
+    if (Number.isNaN(firstNum) || Number.isNaN(secondNum)) {
+        return 'Enter a number';
+    }
+
+    if (result > 100) {
+        return 'Result is too big';
+    }
+
+    return result;
+}
+
+window.calculate = calculate;
