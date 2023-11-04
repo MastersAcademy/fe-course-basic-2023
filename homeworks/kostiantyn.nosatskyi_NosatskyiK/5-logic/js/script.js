801c19c31a9932a38a8eac58f91126b39e93f86a
Kostya N <kostyaa.nosatskiy@gmail.com>
2023-11-04 11:55:01 +0200

    upgrade file index.html, srcipt.js

--- homeworks/kostiantyn.nosatskyi_NosatskyiK/5-logic/js/script.js
+++ homeworks/kostiantyn.nosatskyi_NosatskyiK/5-logic/js/script.js
@@ -1,8 +1,8 @@
-const FIRST_VALUE_ELEMENT = document.getElementById('firstValue');
-const SECOND_VALUE_ELEMENT = document.getElementById('secondValue');
-const OPERATION_ELEMENT = document.getElementById('operation');
-const CALCULATE_BUTTON_ELEMENT = document.getElementById('calculate');
-const RESULT_ELEMENT = document.getElementById('result');
+const FIRST_VALUE_ELEMENT = document.querySelector('[data-firstValue]');
+const SECOND_VALUE_ELEMENT = document.querySelector('[data-secondValue]');
+const OPERATION_ELEMENT = document.querySelector('[data-operation]');
+const CALCULATE_BUTTON_ELEMENT = document.querySelector('[data-calculate]');
+const RESULT_ELEMENT = document.querySelector('[data-result]');
 
 CALCULATE_BUTTON_ELEMENT.addEventListener('click', () => {
     const firstValue = FIRST_VALUE_ELEMENT.value;
