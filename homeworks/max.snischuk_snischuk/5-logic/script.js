ad1a24c3e1649e591c8ec552a6551336e5291ecb
Snischuk Maxim <m.snischuk@gmail.com>
2023-11-04 02:05:24 +0200

    refactor: changed all selectors from ID to Attributes in 5-logic project

--- homeworks/max.snischuk_snischuk/5-logic/script.js
+++ homeworks/max.snischuk_snischuk/5-logic/script.js
@@ -1,8 +1,8 @@
-const FIRST_VALUE_ELEMENT = document.getElementById('firstValue');
-const SECOND_VALUE_ELEMENT = document.getElementById('secondValue');
-const OPERATION_ELEMENT = document.getElementById('operation');
-const CALCULATE_BUTTON_ELEMENT = document.getElementById('calculate');
-const RESULT_ELEMENT = document.getElementById('result');
+const FIRST_VALUE_ELEMENT = document.querySelector('[data-math-value="first"]');
+const SECOND_VALUE_ELEMENT = document.querySelector('[data-math-value="second"]');
+const OPERATION_ELEMENT = document.querySelector('[data-math-operation]');
+const CALCULATE_BUTTON_ELEMENT = document.querySelector('[data-math-calculate]');
+const RESULT_ELEMENT = document.querySelector('[data-math-output]');
 
 CALCULATE_BUTTON_ELEMENT.addEventListener('click', () => {
     const firstValue = FIRST_VALUE_ELEMENT.value;
