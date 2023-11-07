eb6038e8620bfe7062ca08afd83cc1de4a0db113
davidtereshchenko <davakpacaba13@gmail.com>
2023-11-07 13:32:10 +0200

    fixed task with *

--- homeworks/david.tereshchenko_DavidTereshchenko/GameProject/js/calculator.js
+++ homeworks/david.tereshchenko_DavidTereshchenko/GameProject/js/calculator.js
@@ -4,18 +4,21 @@ const visualNumberTwo = document.querySelector('.number__2');
 const visualOperator = document.querySelector('.oper');
 const visualNumberOne = document.querySelector('.number__1');
 const equals = document.querySelector('.equals');
+const visualResult = document.querySelector('.visual-result');
+const countBlock = document.querySelector('.count-block');
 
 btn.onclick = function () {
     const one = Number(document.getElementById('firstNumber').value);
     const two = Number(document.getElementById('secondNumber').value);
     const result = document.querySelector('.result');
 
-    result.style.display = 'flex';
+    countBlock.style.display = 'flex';
 
     let calc;
 
     if (Number.isNaN(one) || Number.isNaN(two)) {
         result.innerHTML = 'Enter a number';
+        visualResult.innerHTML = 'Entar a number';
         visualOperator.innerHTML = '';
         visualNumberOne.innerHTML = '';
         visualNumberTwo.innerHTML = '';
@@ -39,6 +42,7 @@ btn.onclick = function () {
         case '/':
             if (two === 0) {
                 result.innerHTML = 'Number must not be zero';
+                visualResult.innerHTML = 'Entar a number';
                 visualOperator.innerHTML = '';
                 visualNumberOne.innerHTML = '';
                 visualNumberTwo.innerHTML = '';
@@ -50,6 +54,7 @@ btn.onclick = function () {
 
         default:
             result.innerHTML = 'Choose a valid operation';
+            visualResult.innerHTML = 'Entar a number';
             visualOperator.innerHTML = '';
             visualNumberOne.innerHTML = '';
             visualNumberTwo.innerHTML = '';
@@ -59,6 +64,7 @@ btn.onclick = function () {
 
     if (calc > 100) {
         result.innerHTML = 'Result is too big';
+        visualResult.innerHTML = 'Entar a number';
         visualOperator.innerHTML = '';
         visualNumberOne.innerHTML = '';
         visualNumberTwo.innerHTML = '';
@@ -71,6 +77,7 @@ btn.onclick = function () {
     visualNumberTwo.innerHTML = two;
     equals.innerHTML = '=';
     result.innerHTML = calc;
+    visualResult.innerHTML = calc;
 
     return result;
 };
