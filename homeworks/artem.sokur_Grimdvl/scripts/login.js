87499c06c7bf9aa9e8336a6b1475c325f2fee0ae
GrimdvlWork <grimdvl@gmail.com>
2023-11-13 09:31:37 +0200

    exercise with *

--- homeworks/artem.sokur_Grimdvl/scripts/login.js
+++ homeworks/artem.sokur_Grimdvl/scripts/login.js
@@ -2,18 +2,14 @@ const EMAIL_INPUT_ID = 'email';
 const PASSWORD_INPUT_ID = 'password';
 const NOT_A_ROBOT_CHECKBOX_ID = 'checkbox';
 const SUBMIT_BUTTON_ID = 'submit-button';
-const ERRORS_CONTAINER_ID = 'errors-container';
+const ERRORS_CHECKBOX_ID = 'errors-checkbox';
+const ERRORS_PASSWORD_ID = 'errors-password';
+const ERRORS_EMAIL_ID = 'errors-email';
 const RESULT_PAGE_PATH = './success.html';
 
 const submitButton = document.getElementById(SUBMIT_BUTTON_ID);
-// const errorsContainer = document.getElementById(ERRORS_CONTAINER_ID);
 const passwordInput = document.getElementById(PASSWORD_INPUT_ID);
-// const checkboxInput = document.getElementById(NOT_A_ROBOT_CHECKBOX_ID);
 const emailInput = document.getElementById(EMAIL_INPUT_ID);
-
-// Return input value by id.
-// @param {string} elementId
-// @return {string|boolean} input value
 
 function getValueById(elementId) {
     const element = document.getElementById(elementId);
@@ -21,27 +17,28 @@ function getValueById(elementId) {
     return type === 'checkbox' ? element.checked : element.value;
 }
 
-// Add errors to errors container.
-// @param {Object} inputData in format like: { [input_id]: error_text, ... }
-
 function setErrors(inputData) {
-    const errorContainerElement = document.getElementById(ERRORS_CONTAINER_ID);
-    Object.values(inputData).forEach((error) => {
+    Object.entries(inputData).forEach(([inputId, error]) => {
         const errorElement = document.createElement('p');
         errorElement.classList.add('error');
         errorElement.textContent = error;
-        errorContainerElement.appendChild(errorElement);
+
+        const containerId = `errors-${inputId}`;
+        const containerElement = document.getElementById(containerId);
+
+        if (containerElement) {
+            containerElement.appendChild(errorElement);
+        }
     });
 }
 
-// Delete all errors from errors container.
+function deleteErrors(containerId) {
+    const errorContainerElement = document.getElementById(containerId);
 
-function deleteErrors() {
-    const errorContainerElement = document.getElementById(ERRORS_CONTAINER_ID);
-    errorContainerElement.replaceChildren();
+    if (errorContainerElement) {
+        errorContainerElement.replaceChildren();
+    }
 }
-
-// Goes to the page with the result.
 
 function navigateToResultPage() {
     window.location.href = RESULT_PAGE_PATH;
@@ -57,23 +54,31 @@ function passwordLength(password) {
 
 function validateForm() {
     const inputData = {};
-    // place your code here
+
     if (!getValueById(NOT_A_ROBOT_CHECKBOX_ID)) {
-        deleteErrors();
+        deleteErrors(ERRORS_CHECKBOX_ID);
+        deleteErrors(ERRORS_EMAIL_ID);
+        deleteErrors(ERRORS_PASSWORD_ID);
         inputData[NOT_A_ROBOT_CHECKBOX_ID] = 'Checkbox is not checked';
     }
     if (!isEmail(emailInput.value)) {
-        deleteErrors();
+        deleteErrors(ERRORS_CHECKBOX_ID);
+        deleteErrors(ERRORS_EMAIL_ID);
+        deleteErrors(ERRORS_PASSWORD_ID);
         inputData[EMAIL_INPUT_ID] = 'Invalid email format';
     }
     if (!passwordLength(passwordInput.value)) {
-        deleteErrors();
+        deleteErrors(ERRORS_CHECKBOX_ID);
+        deleteErrors(ERRORS_EMAIL_ID);
+        deleteErrors(ERRORS_PASSWORD_ID);
         inputData[PASSWORD_INPUT_ID] = 'Password length should be between 8 and 12 characters';
     }
-    if (Object.keys(inputData).length > 0) {
-        setErrors(inputData);
-    } else {
-        deleteErrors();
+
+    setErrors(inputData);
+    if (Object.keys(inputData).length === 0) {
+        deleteErrors(ERRORS_CHECKBOX_ID);
+        deleteErrors(ERRORS_EMAIL_ID);
+        deleteErrors(ERRORS_PASSWORD_ID);
         navigateToResultPage();
         console.info('Підготовка успішна');
     }
