9f1ff8badc273424f7a22f865425bfd5dd702315
Andrii Morozov <slon88888@ukr.net>
2023-11-18 13:37:46 +0200

    added calculation time

--- homeworks/andrii.morozov_AndriiMorozov88/GamesProject/scripts/logic.js
+++ homeworks/andrii.morozov_AndriiMorozov88/GamesProject/scripts/logic.js
@@ -5,39 +5,55 @@ const button = document.querySelector('[data-button]');
 const resultContainer = document.querySelector('[data-result]');
 const commentContainer = document.querySelector('[data-comment]');
 resultContainer.innerText = 'Result';
+function getDate() {
+    const currentDate = new Date();
+    const options = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    };
+    return currentDate.toLocaleString('ua-UA', options).replace(/\./g, '-');
+}
+function getAbsoluteTime() {
+    const date = new Date();
+    return date.getTime();
+}
 function showResult() {
+    console.log(getAbsoluteTime());
     let result;
+    commentContainer.innerText = '';
+    commentContainer.classList.remove('comment-container--error');
     const firstNumber = Number(firstNumberInput.value);
     const secondNumber = Number(secondNumberInput.value);
     if (firstNumberInput.value.trim() === '' || secondNumberInput.value.trim() === '') {
         resultContainer.innerText = 'Error';
         commentContainer.innerText = 'Enter a number';
+        commentContainer.classList.add('comment-container--error');
         return;
     }
     if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
         resultContainer.innerText = 'Error';
         commentContainer.innerText = 'Enter a number';
+        commentContainer.classList.add('comment-container--error');
+        return;
     }
     switch (operation.value) {
         case '1':
             result = firstNumber + secondNumber;
-            commentContainer.innerText = `${firstNumber} + ${secondNumber} = ${result}`;
             break;
         case '2':
             result = firstNumber - secondNumber;
-            commentContainer.innerText = `${firstNumber} - ${secondNumber} = ${result}`;
             break;
         case '3':
             result = firstNumber * secondNumber;
-            commentContainer.innerText = `${firstNumber} * ${secondNumber} = ${result}`;
             break;
         case '4':
             result = firstNumber / secondNumber;
-            commentContainer.innerText = `${firstNumber} / ${secondNumber} = ${result}`;
             break;
         default:
             result = firstNumber ** secondNumber;
-            commentContainer.innerHTML = `${firstNumber}<sup>${secondNumber}</sup> = ${result}`;
             break;
     }
     resultContainer.innerText = `${result}`;
@@ -45,7 +61,11 @@ function showResult() {
     if (result > 100) {
         resultContainer.innerText = 'Error';
         commentContainer.innerText = 'Too many games';
+        commentContainer.classList.add('comment-container--error');
+        return;
     }
     resultContainer.classList.add('calculator__result--calc');
+    commentContainer.innerText = `Date of calculation: ${getDate()}`;
+    console.log(getAbsoluteTime());
 }
 button.addEventListener('click', showResult);
