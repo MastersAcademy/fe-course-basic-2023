81acdd7925d0f69ac666fa3899655d31c33f7769
GrimdvlDesktop <grimdvl@gmail.com>
2023-11-20 21:26:34 +0300

    simple customReduce + create variable hasValue delete isArray

--- homeworks/artem.sokur_Grimdvl/7-arrays-loops/arrays.js
+++ homeworks/artem.sokur_Grimdvl/7-arrays-loops/arrays.js
@@ -43,11 +43,9 @@ console.log(minNumberOfArr(arrayFive)); // -3
 function sumNumbersOfArr(array) {
     let sumNumbers = 0;
 
-    array.forEach((item) => {
-        if (typeof item === 'number' && !Number.isNaN(item)) {
-            sumNumbers += item;
-        } else if (Array.isArray(item)) {
-            sumNumbers += sumNumbersOfArr(item);
+    array.forEach((number) => {
+        if (typeof number === 'number' && !Number.isNaN(number) && number !== undefined) {
+            sumNumbers += number;
         }
     });
 
@@ -82,12 +80,12 @@ console.log(onlyPositiveNumbers(arrayFive)); // [1, 3, 5];
 
 // exercise *
 
-const numbers = [2, -20, '-10', undefined, '5', 3, 40, NaN, 0, -5, 'One', '', null];
+const numbers = [1, 2, 3, 4, 5];
 
 function customReduce(array, reducer, value) {
-    let nums = value !== undefined ? value : array[0];
-
-    const startIndex = value !== undefined ? 0 : 1;
+    const hasValue = value !== undefined;
+    const startIndex = hasValue ? 0 : 1;
+    let nums = hasValue ? value : array[0];
 
     for (let i = startIndex; i < array.length; i++) {
         nums = reducer(nums, array[i]);
@@ -96,31 +94,13 @@ function customReduce(array, reducer, value) {
 }
 
 const sumAllNumbers = customReduce(numbers, (result, number) => {
-    let num = number;
     let res = result;
-
-    if (typeof num === 'string') {
-        if (num === '') {
-            num = NaN;
-        } else {
-            num = Number(num);
-        }
-    }
-
-    if (!Number.isNaN(num) && num !== undefined && num !== null) {
-        if (num < 0) {
-            res += num * -1;
-        } else {
-            res += num;
-        }
-    }
+    res += number;
 
     return res;
 }, 0);
 
 console.log(sumAllNumbers);
-
-const numbersSeconds = [1, 2, 3, 4, 5];
 
 function customMap(array, mapper) {
     const result = [];
@@ -132,6 +112,6 @@ function customMap(array, mapper) {
     return result;
 }
 
-const multyNumbers = customMap(numbersSeconds, (mapper) => mapper * 2);
+const multyNumbers = customMap(numbers, (mapper) => mapper * 2);
 
 console.log(multyNumbers);
