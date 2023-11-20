e702fa1906d10fa70e050e093ff816ba5f06da6d
LotusElize <handmadenumber@gmail.com>
2023-11-20 22:00:59 +0200

    Homework 7 - Arrays, methods of arrays, loops

---
+++ homeworks/taras.bobov_LotusElize/7-arrays-loops/arrays.js
@@ -0,0 +1,100 @@
+const arrayOne = [3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2, -3, -1];
+const arrayTwo = [-1, -8, -2];
+const arrayThree = [1, 7, 3];
+const arrayFour = [1, undefined, 3, 5, -3];
+const arrayFive = [1, NaN, 3, 5, -3];
+
+function maxNumberOfArr(array) {
+    let maxNumber = Number.NEGATIVE_INFINITY;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (!isNaN(array[i]) && array[i] !== undefined && array[i] > maxNumber) {
+            maxNumber = array[i];
+        }
+    }
+
+    return maxNumber;
+}
+
+// write your code here and instead of array return maxNumber
+console.log(maxNumberOfArr(arrayOne)); // 44
+console.log(maxNumberOfArr(arrayTwo)); // -1
+console.log(maxNumberOfArr(arrayThree)); // 7
+console.log(maxNumberOfArr(arrayFour)); // 5
+console.log(maxNumberOfArr(arrayFive)); // 5
+
+function minNumberOfArr(array) {
+    let minNumber = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minNumber) {
+            minNumber = array[i];
+        }
+    }
+
+    return minNumber;
+
+}
+// write your code here and instead of array return minNumber
+console.log(minNumberOfArr(arrayOne)); // -12
+console.log(minNumberOfArr(arrayTwo)); // -8
+console.log(minNumberOfArr(arrayThree)); // 1
+console.log(minNumberOfArr(arrayFour)); // -3
+console.log(minNumberOfArr(arrayFive)); // -3
+
+function sumNumbersOfArr(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+      continue;
+    }
+
+    sum += array[i];
+  }
+
+  return sum;
+}
+
+console.log(sumNumbersOfArr(arrayOne)); // 32
+console.log(sumNumbersOfArr(arrayTwo)); // -11
+console.log(sumNumbersOfArr(arrayThree)); // 11
+console.log(sumNumbersOfArr(arrayFour)); // 6
+console.log(sumNumbersOfArr(arrayFive)); // 6
+
+function onlyNegativeNumbers(array) {
+  const filteredArray = [];
+
+  for (const element of array) {
+    if (element < 0) {
+      filteredArray.push(element);
+    }
+  }
+
+  return filteredArray;
+}
+
+console.log(onlyNegativeNumbers(arrayOne)); // [-5, -12, -3, -3, -2, -3, -1]
+console.log(onlyNegativeNumbers(arrayTwo)); // [-1, -8, -2]
+console.log(onlyNegativeNumbers(arrayThree)); // []
+console.log(onlyNegativeNumbers(arrayFour)); //  [-3]
+console.log(onlyNegativeNumbers(arrayFive)); // [-3]
+
+function onlyPositiveNumbers(array) {
+    const filteredArray = [];
+
+    for (const element of array) {
+        if (element > 0) {
+            filteredArray.push(element);
+        }
+    }
+
+    return filteredArray;
+}
+ // write your code here and return array only positive numbers
+console.log(onlyPositiveNumbers(arrayOne));// [3, 1, 44, 3, 1, 2, 2, 1, 4]
+console.log(onlyPositiveNumbers(arrayTwo)); // [];
+console.log(onlyPositiveNumbers(arrayThree)); // [1, 7, 3];
+console.log(onlyPositiveNumbers(arrayFour)); // [1, 3, 5];
+console.log(onlyPositiveNumbers(arrayFive)); // [1, 3, 5];
