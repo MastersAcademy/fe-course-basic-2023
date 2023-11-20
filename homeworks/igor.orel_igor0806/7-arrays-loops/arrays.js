939c3eeed9b4f5604cf01abf2438cc639156a5ff
Orel Igor <igor.orel0806@gmail.com>
2023-11-20 12:25:51 +0200

    Homework 7 - Arrays, methods of arrays, loops; igor0806;

---
+++ homeworks/igor.orel_igor0806/7-arrays-loops/arrays.js
@@ -0,0 +1,99 @@
+const arrayOne = [3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2, -3, -1];
+const arrayTwo = [-1, -8, -2];
+const arrayThree = [1, 7, 3];
+const arrayFour = [1, undefined, 3, 5, -3];
+const arrayFive = [1, NaN, 3, 5, -3];
+
+function maxNumberOfArr(array) {
+    let maxNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxNumber) {
+            maxNumber = array[i];
+        }
+    }
+    return maxNumber;
+    // write your code here and instead of array return maxNumber
+}
+
+console.log(maxNumberOfArr(arrayOne)); // 44
+console.log(maxNumberOfArr(arrayTwo)); // -1
+console.log(maxNumberOfArr(arrayThree)); // 7
+console.log(maxNumberOfArr(arrayFour)); // 5
+console.log(maxNumberOfArr(arrayFive)); // 5
+
+function minNumberOfArr(array) {
+    let minNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minNumber) {
+            minNumber = array[i];
+        }
+    }
+    return minNumber;
+    // write your code here and instead of array return minNumber
+}
+
+console.log(minNumberOfArr(arrayOne)); // -12
+console.log(minNumberOfArr(arrayTwo)); // -8
+console.log(minNumberOfArr(arrayThree)); // 1
+console.log(minNumberOfArr(arrayFour)); // -3
+console.log(minNumberOfArr(arrayFive)); // -3
+
+function sumNumbersOfArr(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== undefined && !Number.isNaN(array[i])) {
+            sum += array[i];
+        }
+    }
+    return sum;
+    // write your code here and instead of array return sum of numbers
+}
+
+console.log(sumNumbersOfArr(arrayOne)); // 32
+console.log(sumNumbersOfArr(arrayTwo)); // -11
+console.log(sumNumbersOfArr(arrayThree)); // 11
+console.log(sumNumbersOfArr(arrayFour)); // 6
+console.log(sumNumbersOfArr(arrayFive)); // 6
+
+function onlyNegativeNumbers(array) {
+    return array.filter((num) => num < 0);
+    // write your code here and return array only with negative numbers
+}
+
+console.log(onlyNegativeNumbers(arrayOne)); // [-5, -12, -3, -3, -2, -3, -1]
+console.log(onlyNegativeNumbers(arrayTwo)); // [-1, -8, -2]
+console.log(onlyNegativeNumbers(arrayThree)); // []
+console.log(onlyNegativeNumbers(arrayFour)); //  [-3]
+console.log(onlyNegativeNumbers(arrayFive)); // [-3]
+
+function onlyPositiveNumbers(array) {
+    return array.filter((num) => num > 0);
+    // write your code here and return array only positive numbers
+}
+
+console.log(onlyPositiveNumbers(arrayOne));// [3, 1, 44, 3, 1, 2, 2, 1, 4]
+console.log(onlyPositiveNumbers(arrayTwo)); // [];
+console.log(onlyPositiveNumbers(arrayThree)); // [1, 7, 3];
+console.log(onlyPositiveNumbers(arrayFour)); // [1, 3, 5];
+console.log(onlyPositiveNumbers(arrayFive)); // [1, 3, 5];
+
+// function customReduce(array, reducer, initialValue) {
+//     let acc = initialValue;
+//     const previosValue = initialValue !== undefined ? 0 : 1;
+//     for (let i = previosValue; i < array.length; i++) {
+//         acc = reducer(acc, array[i], i, array);
+//     }
+//
+//     return acc;
+// }
+// не работает map;
+// function customMap(array, map) {
+//     let result = [];
+//     for (let i = 0; i < array.length; i++) {
+//         result = map(array[i], i, array);
+//     }
+//     return result;
+// }
+//
+// console.log(customMap());
+// console.log(customReduce());
