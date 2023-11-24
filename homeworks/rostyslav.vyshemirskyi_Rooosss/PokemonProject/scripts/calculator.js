aa68b7c1a244def9e9cd71bad36bf05b077391cd
Rooosss <vyshemirskyirostyslav@gmail.com>
2023-11-24 23:09:35 +0200

    added pokemon cards using javascript

--- homeworks/rostyslav.vyshemirskyi_Rooosss/PokemonProject/scripts/calculator.js
+++ homeworks/rostyslav.vyshemirskyi_Rooosss/PokemonProject/scripts/calculator.js
@@ -3,7 +3,7 @@
  * @param firstValue string with first number
  * @param secondValue string with second number
  * @param operation string with operation symbol
- * @returns {string|pokemons} result of calculation or error message:
+ * @returns {string} result of calculation or error message:
  * - if firstValue or secondValue is not a number, return 'Enter a number'
  * - if operation is not '+', '-', '*', '/', return 'Choose a valid operation'
  * - if the result is more than 100, return 'Result is too big'
