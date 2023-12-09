e4cecdbed8926b68cd20c97973144bbf4d89ee81
Kuromai <arconiel456@gmail.com>
2023-12-09 13:12:58 +0200

    feat: added a request to the api and changed the filtering function

--- homeworks/vladyslav.brusentsov_vBrusentsov/GamesProject/script/games.js
+++ homeworks/vladyslav.brusentsov_vBrusentsov/GamesProject/script/games.js
@@ -1,4 +1,5 @@
 const cardContainer = document.querySelector('[data-type="card-container"]');
+const loadingOverlay = document.getElementById('loading-overlay');
 
 function createCardElement(game) {
     const templateByAttribute = document.querySelector('[data-card-template]');
@@ -25,114 +26,111 @@ function renderCards(container, dataGames) {
     container.appendChild(fragment);
 }
 
-async function getAllGame() {
+async function getAllGame(query = {}, localParams = {}) {
     try {
-        await fetch('https://mmo-games.p.rapidapi.com/games', {
+        const queryParams = new URLSearchParams(query);
+        loadingOverlay.style.display = 'flex';
+        renderCards(cardContainer, []);
+        /* If you have good ethernet and you get very fast response you can uncomment next row */
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        await fetch(`https://mmo-games.p.rapidapi.com/games?${queryParams.toString()}`, {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': '1c3169c707mshb51bff34cbc9ff6p1749b9jsn648a19134256',
                 'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com',
             },
         }).then((response) => response.json()).then((data) => {
-            const games = data.slice(0, 50);
-            return renderCards(cardContainer, games);
+            if (data.status === 0) {
+                return renderCards(cardContainer, []);
+            }
+            let games = localParams['sort-by-type'] === 'old-games' ? data.reverse() : data;
+            if (localParams.search) {
+                console.log(games);
+                games = games.filter((game) => game.title.toLowerCase().includes(localParams.search)
+                || game.short_description.toLowerCase().includes(localParams.search));
+            }
+
+            return renderCards(cardContainer, games.slice(0, 50));
+        }).finally(() => {
+            loadingOverlay.style.display = 'none';
+            console.log(loadingOverlay.style.display);
         });
     } catch (error) {
         console.error(error);
     }
 }
 
-async function getRadioButtonFilter(event) {
-    try {
-        const selectedValue = event.target.value;
-        await fetch(`https://mmo-games.p.rapidapi.com/games?platform=${selectedValue}`, {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '1c3169c707mshb51bff34cbc9ff6p1749b9jsn648a19134256',
-                'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com',
-            },
-        }).then((response) => response.json()).then((data) => {
-            const games = data.slice(0, 50);
-            renderCards(cardContainer, games);
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function getSelectFilterGames() {
-    try {
-        const genreSelect = document.getElementById('game-properties');
-        const selectedGenre = genreSelect.value;
-        await fetch(`https://mmo-games.p.rapidapi.com/games?category=${selectedGenre}`, {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '1c3169c707mshb51bff34cbc9ff6p1749b9jsn648a19134256',
-                'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com',
-            },
-        }).then((response) => response.json()).then((data) => {
-            const games = data.slice(0, 50);
-            renderCards(cardContainer, games);
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function getSortGamesByDate(event) {
-    try {
-        console.log(event.target.value);
-        if (event.target.value === 'New First') {
-            const response = await fetch(' https://www.mmobomb.com/api1/games?&sort-by=release-date', {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': '1c3169c707mshb51bff34cbc9ff6p1749b9jsn648a19134256',
-                    'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com',
-                },
-            });
-            const data = response.json();
-            const games = data.slice(0, 50);
-            renderCards(cardContainer, games);
-        }
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function getSearchBarFilteredGames() {
-    const searchBar = document.getElementById('search').value.toLowerCase();
-
-    const response = await fetch('https://mmo-games.p.rapidapi.com/games', {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '1c3169c707mshb51bff34cbc9ff6p1749b9jsn648a19134256',
-            'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com',
-        },
-    });
-    const data = await response.json();
-    const games = data.slice(0, 50);
-    const searchBarFilter = games.filter((game) => game.title.toLowerCase().includes(searchBar)
-       || game.short_description.toLowerCase().includes(searchBar));
-    renderCards(cardContainer, searchBarFilter);
+async function getSearchBarFilteredGames(localParams) {
+    localParams.search = document.getElementById('search').value.toLowerCase();
+    console.log(localParams);
+    getAllGame(undefined, localParams);
 }
 
 function init() {
     const radioButtonsFilterPlatform = document.querySelector('.radio-filter');
-    const radioButtonsSortByDate = document.querySelectorAll('.radio-date-sort');
+    const radioButtonsSortByDate = document.querySelector('.radio-date-sort');
     const applyButton = document.getElementById('applyButton');
     const searchBar = document.getElementById('search');
     const genreSelect = document.getElementById('game-properties');
     // event listeners
-    radioButtonsSortByDate.forEach((checkbox) => {
-        checkbox.addEventListener('change', getSortGamesByDate);
+    let radioFilterState = {};
+    const queryParams = {};
+    const localParams = {};
+    radioButtonsSortByDate.addEventListener('click', (event) => {
+        const activeInputSortByDate = event.target;
+        if (activeInputSortByDate.tagName === 'INPUT') {
+            if (activeInputSortByDate.checked
+                && radioFilterState[activeInputSortByDate.id]) {
+                activeInputSortByDate.checked = false;
+            }
+            radioFilterState = {
+                [activeInputSortByDate.id]: activeInputSortByDate.checked,
+            };
+            if (activeInputSortByDate.checked) {
+                queryParams['sort-by'] = 'release-date';
+                localParams['sort-by-type'] = activeInputSortByDate.id;
+            } else {
+                queryParams['sort-by'] = '';
+                localParams['sort-by-type'] = activeInputSortByDate.id;
+            }
+            getAllGame(queryParams, localParams);
+        }
     });
-    radioButtonsFilterPlatform.addEventListener('change', getRadioButtonFilter);
+
+    radioButtonsFilterPlatform.addEventListener('click', (event) => {
+        const activeInputPlatformFilter = event.target;
+        if (activeInputPlatformFilter.tagName === 'INPUT') {
+            console.log(activeInputPlatformFilter.id);
+            if (activeInputPlatformFilter.checked
+                && radioFilterState[activeInputPlatformFilter.id]) {
+                activeInputPlatformFilter.checked = false;
+            }
+            radioFilterState = {
+                [activeInputPlatformFilter.id]: activeInputPlatformFilter.checked,
+            };
+            if (activeInputPlatformFilter.checked) {
+                queryParams.platform = event.target.value;
+            } else {
+                delete queryParams.platform;
+            }
+            getAllGame(queryParams);
+        }
+    });
     applyButton.addEventListener('click', getSearchBarFilteredGames);
     searchBar.addEventListener('keyup', getSearchBarFilteredGames);
-    genreSelect.addEventListener('change', getSelectFilterGames);
-
+    genreSelect.addEventListener('change', () => {
+        if (genreSelect.value === 'default') {
+            delete queryParams.category;
+        } else {
+            queryParams.category = genreSelect.value;
+        }
+        getAllGame(queryParams);
+    });
+    console.log(loadingOverlay.style.display);
+    console.log(loadingOverlay.style.display);
     // return getSortGamesByDate() || getSearchBarFilteredGames()|| getSelectFilterGames();
-    return getAllGame() || getSelectFilterGames() || getSortGamesByDate();
+    return getAllGame();
 }
 
 document.addEventListener('DOMContentLoaded', init);
