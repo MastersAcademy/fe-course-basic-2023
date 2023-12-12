475e18a4170a5e5a72aa6252898bbcc40bb9787c
Taras Ternovski <128043439+LotusElize@users.noreply.github.com>
2023-12-12 23:41:53 +0200

    Update games.js

--- homeworks/taras.bobov_LotusElize/GamesProject/js/games.js
+++ homeworks/taras.bobov_LotusElize/GamesProject/js/games.js
@@ -4,7 +4,6 @@ const checkboxNew = document.querySelector('[data-filter="new"]');
 const checkboxOld = document.querySelector('[data-filter="old"]');
 let loadingOverlay;
 
-// Wait for the DOM to be fully loaded before accessing elements
 document.addEventListener('DOMContentLoaded', () => {
     loadingOverlay = document.querySelector('.loading');
 });
@@ -16,31 +15,28 @@ function showLoadingOverlay() {
 }
 
 function truncateText(text, maxLength) {
-    const numModifiedCharacters = 50; // Задайте бажане значення тут
+    const numModifiedCharacters = 50;
     const words = text.split(' ');
     let truncatedText = '';
     let currentLength = 0;
 
-    for (const word of words) {
+    words.forEach((word) => {
         if (currentLength + word.length <= maxLength) {
-            truncatedText += word + ' ';
-            currentLength += word.length + 1; // +1 за пробіл
-        } else {
-            break;
+            truncatedText += `${word} `;
+            currentLength += word.length + 1;
         }
-    }
+    });
 
     if (truncatedText.length < text.length) {
-        truncatedText = truncatedText.trim() + '...';
+        truncatedText = `${truncatedText.trim()}...`;
     }
 
     if (numModifiedCharacters && truncatedText.length > numModifiedCharacters) {
-        truncatedText = truncatedText.substring(0, numModifiedCharacters) + '...';
+        truncatedText = `${truncatedText.substring(0, numModifiedCharacters)}...`;
     }
 
     return truncatedText;
 }
-
 
 async function createCardElement(game) {
     const cardCopy = document.importNode(document.querySelector('[data-type="card-template"]').content, true);
@@ -48,7 +44,7 @@ async function createCardElement(game) {
     cardCopy.querySelector('.card [data-card-img]').src = game.thumbnail;
     cardCopy.querySelector('.card [data-card-title]').textContent = game.title;
 
-    const shortDescription = truncateText(game.short_description, 150); // Adjust the length as needed
+    const shortDescription = truncateText(game.short_description, 150);
     cardCopy.querySelector('.card [data-card-description]').textContent = shortDescription;
 
     cardCopy.querySelector('.card [data-card-genre]').textContent = game.genre;
@@ -60,7 +56,6 @@ async function createCardElement(game) {
     return cardCopy;
 }
 
-
 async function renderCards(container, games) {
     const containerElement = document.querySelector(`[data-container="${container}"]`);
 
@@ -69,7 +64,7 @@ async function renderCards(container, games) {
         return;
     }
 
-    containerElement.innerHTML = ''; // Clearing innerHTML
+    containerElement.innerHTML = '';
 
     games.forEach(async (game) => {
         const card = await createCardElement(game);
@@ -82,6 +77,7 @@ function hideLoadingOverlay() {
         loadingOverlay.style.display = 'none';
     }
 }
+
 async function fetchData() {
     try {
         showLoadingOverlay();
@@ -111,7 +107,6 @@ async function fetchData() {
     }
 }
 
-// Function to filter and update displayed cards
 function updateCards() {
     const isNewChecked = checkboxNew.checked;
     const isOldChecked = checkboxOld.checked;
@@ -131,23 +126,7 @@ function updateCards() {
     renderCards('game-cards', filteredGames);
 }
 
-// Event listeners for checkbox changes
 checkboxNew.addEventListener('change', updateCards);
 checkboxOld.addEventListener('change', updateCards);
 
-// Initial data fetch and display
 fetchData();
-
-
-// Function to show the loading overlay
-function showLoadingOverlay() {
-    document.getElementById('loading-overlay').classList.add('active');
-}
-
-// Function to hide the loading overlay
-function hideLoadingOverlay() {
-    document.getElementById('loading-overlay').classList.remove('active');
-}
-
-// Call showLoadingOverlay() before making your API request
-// Call hideLoadingOverlay() when your API request is complete
