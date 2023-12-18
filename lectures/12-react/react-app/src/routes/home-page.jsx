efd45f653dc1cc665a1fbcfd3d2187e7d83fe104
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-12-19 00:21:49 +0200

    Added react project

---
+++ lectures/12-react/react-app/src/routes/home-page.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header/Header.jsx";
+import Home from "../components/home/Home.jsx";
+
+const HomePage = () => {
+    return <>
+        <Header />
+        <Home />
+       {/*<h1>Home Page</h1>*/}
+    </>
+}
+
+export default HomePage
