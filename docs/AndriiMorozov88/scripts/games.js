f97706ca94276d99d9c0d213d740e260ea8672b7
Andrii Morozov <slon88888@ukr.net>
2024-01-03 14:51:07 +0200

    added styles to calculator css

--- docs/AndriiMorozov88/scripts/games.js
+++ docs/AndriiMorozov88/scripts/games.js
@@ -45,10 +45,7 @@ async function getGamesArray() {
     } else {
         games = await allGames;
     }
-    // for (let count = 0; count < 100000; count++) {
-    //     console.log(count);
-    //     console.clear();
-    // }
+    for (let count = 0; count < 1000000000; count++);
     return games;
 }
 function createCardElement(game, array) {
