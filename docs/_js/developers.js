a3557a3493f8a24b5fe2293e62f7375896522b10
Andrii Morozov <slon88888@ukr.net>
2024-03-01 14:14:14 +0200

    added developers.js

---
+++ docs/_js/developers.js
@@ -0,0 +1,14 @@
+export const developers = [
+    { firstName: 'Ekaterina', LastName: 'Sharova', action: './kostiantyn.nosatskyi/PokemonProject/index.html' },
+    { firstName: 'Kostiantyn', LastName: 'Nosatskyi', action: './rogerosha/index.html' },
+    { firstName: 'Max', LastName: 'Snischuk', action: './snischuk/index.html' },
+    { firstName: 'Rostyslav', LastName: 'Vyshemirskyi', action: './rooosss/index.html' },
+    { firstName: 'Ihor', LastName: 'Zapisochniy', action: './IgorFromUA/index.html' },
+    { firstName: 'Artur', LastName: 'Kobyliatsky', action: './Allocene/index.html' },
+    { firstName: 'Serhii', LastName: 'Noskov', action: './SerhiiNSN/index.html' },
+    { firstName: 'Dmytro', LastName: 'Shevchenko', action: './dmytro-shevchenko1/GamesProject/index.html' },
+    { firstName: 'Sviatoslav', LastName: 'Taranenko', action: './SviatoslavTaranenko/index.html' },
+    { firstName: 'Mariia', LastName: 'Demchenko', action: './demasha0908/index.html' },
+    { firstName: 'Andrii', LastName: 'Morozov', action: './AndriiMorozov88/about.html' },
+    { firstName: 'Bogdan', LastName: 'Yatsentiuk', action: './Bogdandrummer/index.html' },
+];
