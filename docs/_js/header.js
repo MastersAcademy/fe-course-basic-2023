113afc88f8bd2337b46727146c210fdcb8106dda
Bogdan <104357137+Bogdandrummer@users.noreply.github.com>
2024-03-02 18:39:17 +0200

    added d__flex class for header.js, changed favicon

--- docs/_js/header.js
+++ docs/_js/header.js
@@ -2,10 +2,7 @@ const burgerButton = document.querySelector('[data-burger]');
 const dataMenuList = document.querySelector('[data-menu__list]');
 const burgerAction = () => {
     if (dataMenuList.style.display === '' || dataMenuList.style.display === 'none') {
-        dataMenuList.style.display = 'flex';
-    } else
-    if (dataMenuList.style.display === 'flex') {
-        dataMenuList.style.display = '';
+        dataMenuList.classList.toggle('d__flex');
     }
 };
 burgerButton.addEventListener('click', burgerAction);
