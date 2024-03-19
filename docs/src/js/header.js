8304b662b432db899383a735ecb082f11ccd1bd2
Andrii Morozov <slon88888@ukr.net>
2024-03-19 10:34:03 +0200

    add comments

--- docs/src/js/header.js
+++ docs/src/js/header.js
@@ -1,32 +1,32 @@
-const burgerButton = document.querySelector('[data-burger]');
-const dataMenuList = document.querySelector('[data-menu__list]');
-const liftUp = document.querySelector('[data-lift_up]');
-const liftPoint = document.querySelector('[data-lift_point]');
+// const burgerButton = document.querySelector('[data-burger]');
+// const dataMenuList = document.querySelector('[data-menu__list]');
+// const liftUp = document.querySelector('[data-lift_up]');
+// const liftPoint = document.querySelector('[data-lift_point]');
 
-const burgerAction = () => {
-    if (dataMenuList.style.display === '' || dataMenuList.style.display === 'none') {
-        dataMenuList.classList.toggle('d__flex');
-    }
-};
+// const burgerAction = () => {
+//     if (dataMenuList.style.display === '' || dataMenuList.style.display === 'none') {
+//         dataMenuList.classList.toggle('d__flex');
+//     }
+// };
 
-const checkScroll = () => {
-    if (window.scrollY > 500 && window.innerWidth < 602) {
-        liftUp.style.right = '7';
-    } else {
-        liftUp.style.right = '-50';
-    }
-};
-const arrowAppearance = () => {
-    if (window.innerWidth < 602 || window.innerWidth > 602) {
-        checkScroll();
-    }
-};
+// const checkScroll = () => {
+//     if (window.scrollY > 500 && window.innerWidth < 602) {
+//         liftUp.style.right = '7';
+//     } else {
+//         liftUp.style.right = '-50';
+//     }
+// };
+// const arrowAppearance = () => {
+//     if (window.innerWidth < 602 || window.innerWidth > 602) {
+//         checkScroll();
+//     }
+// };
 
-const handleButtonClick = () => {
-    liftPoint.scrollIntoView({ block: 'center', behavior: 'smooth' });
-};
+// const handleButtonClick = () => {
+//     liftPoint.scrollIntoView({ block: 'center', behavior: 'smooth' });
+// };
 
-liftUp.addEventListener('click', handleButtonClick);
-window.addEventListener('resize', arrowAppearance);
-window.addEventListener('scroll', arrowAppearance);
-burgerButton.addEventListener('click', burgerAction);
+// liftUp.addEventListener('click', handleButtonClick);
+// window.addEventListener('resize', arrowAppearance);
+// window.addEventListener('scroll', arrowAppearance);
+// burgerButton.addEventListener('click', burgerAction);
